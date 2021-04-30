c309e9788a6402b471ab8d12bf5aeecd3c1b4544
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-04-30 14:35:52 +0530

    Update Info.py

--- DaisyX/modules/assistant/Info.py
+++ DaisyX/modules/assistant/Info.py
@@ -1,7 +1,7 @@
 from telethon.utils import pack_bot_file_id
-from ULTRAX import xbot, devs as DEVS
+from DaisyX.functions.DaisyX import xbot, devs as DEVS
 from telethon import events
-from ULTRA import bot
+from DaisyX import bot
 
 @xbot.on(events.NewMessage(pattern="/id ?(.*)"))
 async def _(event):
