0de002f4f5b1b21d9c59f956b977f6166323226d
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-04-30 15:03:06 +0530

    Update functions.py

--- DaisyX/helpers/functions.py
+++ DaisyX/helpers/functions.py
@@ -9,7 +9,7 @@ from os import getcwd
 from os.path import basename, join
 from textwrap import wrap
 from typing import Optional, Tuple
-from ULTRA.events import get_readable_time
+from DaisyX.events import get_readable_time
 
 try:
     from colour import Color as asciiColor
