b1ec249f3893e55e07b9e955099d1f4351b35e15
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-04-30 18:48:49 +0530

    Update __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -158,7 +158,7 @@ if bool(ENV):
     # Upstream Repo
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/LEGENDXOP/LEGEND-BOT.git")
+    "https://github.com/TeamDaisyX/DaisyX-UB.git")
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
