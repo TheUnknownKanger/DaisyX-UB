e87084d3658a4084e133ba2fcc053ea26cbff361
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-04-30 15:08:52 +0530

    Add files via upload

---
+++ DaisyX/__main__.py
@@ -0,0 +1,130 @@
+import os
+try:
+  from LEGENDX import id, ID, devs, rd, wt
+except:
+  os.system("pip install LEGENDX==0.0.21")
+  from LEGENDX import id, ID, devs
+finally:
+  print ("ULTRA X IS STARTING WITH TELETHON") 
+from ULTRAX import xbot
+from ULTRA import bot, CMD_HELP
+from sys import argv
+os.system("pip install telethon==1.19.0")
+import sys
+import os
+os.system("pip install google_trans_new")
+import glob
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+import os
+from telethon import TelegramClient, Button
+from var import Var
+from ULTRA.utils import load_module, load_pro
+from ULTRA import LOAD_PLUG, BOTLOG_CHATID
+from pathlib import Path
+import asyncio
+TOKEN = os.environ.get("TG_BOT_TOKEN", None)
+import telethon.utils
+EXTRA_PLUGS = os.environ.get("EXTRA_PLUGS", False)
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me() 
+    bot.uid = telethon.utils.get_peer_id(bot.me)
+ONLINE_ALERT = os.environ.get("ONLINE_ALERT")
+os.system("pip install LEGENDX==0.0.21")
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
+        bot.tgbot = TelegramClient(
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished with no errors")
+        print("Starting Userbot")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
+    else:
+        bot.start()
+
+import glob
+
+
+
+path = 'ULTRA/plugins/assistant/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_pro(shortname.replace(".py", ""))
+
+
+if  EXTRA_PLUGS == True:
+    os.system("git clone https://github.com/ULTRA-OP/ULTRA_PLUGS.git ./ULTRA/plugins/")
+    path = "ULTRA/plugins/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem
+            try:
+                load_module(plugin_name.replace(".py", ""))
+                if not plugin_name.startswith("__") or plugin_name.startswith("_"):
+                    print ('INSTALLING ALL MODULES', plugin_name)
+            except:
+                pass
+
+else:
+  path = 'ULTRA/plugins/*.py'
+  files = glob.glob(path)
+  for name in files:
+      with open(name) as f:
+          path1 = Path(f.name)
+          shortname = path1.stem
+          load_module(shortname.replace(".py", ""))
+
+
+import ULTRA._core
+import os
+print("UltraX is Up and Awake! ©️ TeamUltraX 2021")
+async def legend():
+  pro = await xbot.get_me()
+  legend = await bot.get_me()
+  LEGENDX = f"""
+**Sᴏᴍᴇᴛʜɪɴɢ Hᴀᴘᴘᴇɴᴇᴅ ! Lᴇᴛs Cʜᴇᴄᴋ** 🤔 
+
+`☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎ ☟︎︎︎`
+
+**Dɪɴɢ Dᴏɴɢ...** `.\./.\` **Tɪɴɢ Tᴏɴɢ...** `./.\./` **UʟᴛʀᴀX Hᴀs Bᴇᴇɴ Dᴇᴘʟᴏʏᴇᴅ !!**
+
+**Pɪɴɢ Pᴏɴɢ...**
+
+**➥ Mᴀsᴛᴇʀ** `➪` **@{legend.username}**
+**➥ Assɪsᴛᴀɴᴛ** `➪` **@{pro.username}**
+**➥ Sᴜᴘᴘᴏʀᴛ** `➪` **@UltraXchaT**
+**➥ Cʜᴀɴɴᴇʟ** `➪` **@UltraX_SupporT**
+
+**Cʜᴇᴄᴋ ᴍᴏɪ Pɪɴɢ ᴛɪᴍᴇ ʙʏ** `.ping` **[Fᴏʀ UsᴇʀBᴏᴛ] or** `/ping` **[Fᴏʀ Assɪsᴛᴀɴᴛ]**
+"""
+  if ONLINE_ALERT:
+    try:
+      PROBOYX = [[Button.inline("Hᴇʀᴏᴋᴜ Vᴀʀs", data='ass_back')]]
+      
+      await xbot.send_message(bot.me.id, LEGENDX, buttons=PROBOYX)
+    except:
+       pass
+  else:
+      print("YOUR BOT DEPLOYED SUCCESSFULLY")
+
+bot.loop.run_until_complete(legend())
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+    
+else:
+    bot.run_until_disconnected()
+    
+
