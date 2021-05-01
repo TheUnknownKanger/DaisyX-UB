b6a7abe3e3d99d49d5f7d6e71bca00b5d7127e94
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-01 16:35:48 +0530

    Update antiflood_sql.py

--- DaisyX/modules/sql_helper/antiflood_sql.py
+++ DaisyX/modules/sql_helper/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, String
 
-from ULTRA.plugins.sql_helper import BASE, SESSION
+from DaisyX.modules.sql_helper import BASE, SESSION
 
 DEF_COUNT = 0
 DEF_LIMIT = 0
