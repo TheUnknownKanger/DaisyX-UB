53a6ba9e72f5a24da27ee1cff9025102b707ad97
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-01 13:27:23 +0530

    Update __init__.py

--- DaisyX/modules/__init__.py
+++ DaisyX/modules/__init__.py
@@ -1,6 +1,6 @@
-from ULTRA.helpers import functions as simpdef
-from LEGENDX import *
-from LEGENDX.ULTRAX import *
+from DaisyX.helpers import functions as simpdef
+from Assist import *
+from Assist.DAISYX import *
 simpmusic = simpdef.simpmusic 
 simpmusicvideo = simpdef.simpmusicvideo
 
