05dcac58640b7b25d2684cb142938b12b465e68c
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-01 16:21:34 +0530

    Update notes_sql.py

--- DaisyX/modules/sql_helper/notes_sql.py
+++ DaisyX/modules/sql_helper/notes_sql.py
@@ -1,5 +1,5 @@
 try:
-    from ULTRA.modules.sql_helper import SESSION, BASE
+    from DaisyX.modules.sql_helper import SESSION, BASE
 except ImportError:
     raise Exception("Hello!")
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
