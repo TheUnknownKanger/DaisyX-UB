14b5c8cc3004f0c0a68c33271da215ff48358b5d
InukaAsith <inukaasith7@gmail.com>
2021-05-02 09:41:54 +0000

    Auto Fixes: Code formatted

--- DaisyX/utils.py
+++ DaisyX/utils.py
@@ -16,8 +16,8 @@ from telethon import events
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from DaisyX import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
 from Assist import xbot
+from DaisyX import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
 from DaisyX.helpers.exceptions import CancelProcess
 from var import Var
 
