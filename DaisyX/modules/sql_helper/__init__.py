42c7e9d75ee61498f5a2826e8d0008bf422a607d
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-03 15:05:23 +0530

    Update __init__.py

--- DaisyX/modules/sql_helper/__init__.py
+++ DaisyX/modules/sql_helper/__init__.py
@@ -9,7 +9,7 @@ from var import Var
 
 
 def start() -> scoped_session:
-    engine = create_engine(Var.DB_URI, client_encoding="utf8")
+    engine = create_engine(Var.DATABASE_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
