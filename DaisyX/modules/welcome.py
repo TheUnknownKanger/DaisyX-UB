4f692a7c61f17840119aaf5df8032b00ecb8abb5
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-03 16:42:36 +0530

    Update welcome.py

--- DaisyX/modules/welcome.py
+++ DaisyX/modules/welcome.py
@@ -1,7 +1,7 @@
 from telethon import events
 from telethon.utils import pack_bot_file_id
 
-from DaisyX.plugins.sql_helper.welcome_sql import (
+from DaisyX.modules.sql_helper.welcome_sql import (
     add_welcome_setting,
     get_current_welcome_settings,
     rm_welcome_setting,
