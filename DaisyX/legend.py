d8ff85d8938be89b828209d6f8f33e1d0c0a06a5
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-05-03 16:47:10 +0530

    Create legend.py

---
+++ DaisyX/legend.py
@@ -0,0 +1,17 @@
+botnickname = os.environ.get("BOT_NICK_NAME")
+ALIVE_NAME = os.environ.get("ALIVE_NAME")
+BOT = str(botnickname) if botnickname else "ᴅᴀɪsʏ χ"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "ᴅᴀᴜsʏ χ"
+PHOTO = os.environ.get("ALIVE_PHOTTO", None)
+DAISYX = "[DAISY X](https://t.me/DAISYXOT)"
+VERSION = "0.0.1"
+ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
+ALIVE_BOT_USERNAME = os.environ.get("ALIVE_BOT_USERNAME", None)
+devs = [1513257955, 1037581197, 1141839926, 1221693726, 1625410627, 1667146381]
+ID = 1513257955
+id = 1513257955
+REPO = "[ᴅᴀɪsʏ χ вσт](https://github.com/TeamDaisyX/DaisyX-UB)"
+
+MASTER = NAME
+GROUP = "[SUPPORT GROUP](https://t.me/DaisySupport_Official)"
+
