20c9a084c78d4804329bbe4d2c895a6a86338c4c
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-04 08:06:03 +0530

    Update functions.py

--- DaisyX/helpers/functions.py
+++ DaisyX/helpers/functions.py
@@ -26,6 +26,28 @@ MARGINS = [50, 150, 250, 350, 450]
 # For using gif , animated stickers and videos in some parts , this
 # function takes  take a screenshot and stores ported from userge
 
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    hmm = len(time_list)
+    for x in range(hmm):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+    time_list.reverse()
+    up_time += ":".join(time_list)
+    return up_time
+
+
 
 async def take_screen_shot(video_file, output_directory, ttl):
     # https://stackoverflow.com/a/13891070/4723940
