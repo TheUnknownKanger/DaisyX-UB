88b566d73284b411afc2a77fe9e63694fe97a544
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-04 14:35:59 +0530

    Update _inline.py

--- DaisyX/modules/_inline.py
+++ DaisyX/modules/_inline.py
@@ -154,7 +154,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             await event.edit(buttons=buttons)
         else:
             reply_pop_up_alert = (
-                "Please get your own Userbot from @TeleBotHelp , and don't use mine!"
+                "Please get your own Userbot from @DaisySupport_Official , and don't use mine!"
             )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
