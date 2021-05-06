2b50d975d9d34f6da0d99a3eccdcb06700b2f9eb
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-06 10:39:07 +0530

    Update restart.py

--- DaisyX/modules/restart.py
+++ DaisyX/modules/restart.py
@@ -5,6 +5,7 @@ from DaisyX.utils import admin_cmd, sudo_cmd
 @bot.on(admin_cmd(pattern="restart"))
 async def _(event):
     await bot.send_message(event.chat.id, '`Wait restarting`')
+    await event.delete() 
     asyncio.sleep(0.5)
     await event.edit('`Restarted successfully`')
     os.execl(sys.executable, sys.executable, *sys.argv)
