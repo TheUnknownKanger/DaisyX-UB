42666d18f89609e6c13bdf5d9109ee466b7f9621
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-06 10:34:44 +0530

    Update restart.py

--- DaisyX/modules/restart.py
+++ DaisyX/modules/restart.py
@@ -3,11 +3,11 @@ import os,re
 from telethon.tl.custom import Button 
 from telethon import events, errors, custom, functions
 import os, re, sys, asyncio
-from DaisyX import admin_cmd
+from DaisyX.utils import admin_cmd, sudo_cmd
 
 @bot.on(admin_cmd(pattern="restart"))
 async def _(event):
-    await bot.send_message(event.chat.id, '`Weit restarting`')
+    await bot.send_message(event.chat.id, '`Wait restarting`')
     asyncio.sleep(0.5)
     await event.edit('`Restarted successfully`')
     os.execl(sys.executable, sys.executable, *sys.argv)
