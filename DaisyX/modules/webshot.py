6367129407219b91f0be4b1e0dda5f3c937c37eb
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-06 08:44:50 +0530

    Update webshot.py

--- DaisyX/modules/webshot.py
+++ DaisyX/modules/webshot.py
@@ -15,7 +15,7 @@ async def _(event):
     if event.fwd_from:
         return
     if SCREEN_SHOT_LAYER_ACCESS_KEY is None:
-        await event.edit ('''Need to get an API key from https://screenshotlayer.com/product and add it manually from heroku Or do via   
+        await event.edit('''Need to get an API key from https://screenshotlayer.com/product and add it manually from heroku Or do via   
 .set var SCREEN_SHOT_LAYER_ACCESS_KEY type api key here\nModule stopping!''')
         return
     await event.edit("Processing ...")
