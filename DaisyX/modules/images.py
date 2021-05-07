50c0ad87c152c68eef7ea78785c6832e287e6f7c
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-07 09:47:02 +0530

    Update images.py

--- DaisyX/modules/images.py
+++ DaisyX/modules/images.py
@@ -1,38 +1,36 @@
-"""Download & Upload Images on Telegram\n
-Syntax: `.img <Name>` or `.img (replied message)`
-\n Upgraded and Google Image Error Fixed
-"""
-
+# image search for catuserbot
 import os
 import shutil
-from re import findall
 
-from DaisyX.google_imgs import googleimagesdownload
-from DaisyX.utils import admin_cmd
+from DaisyX.google_image import googleimagesdownload
+from DaisyX.utils import admin_cmd, sudo_cmd
+from DaisyX import CMD_HELP
 
-
-@borg.on(admin_cmd(pattern="img ?(.*)"))
+@bot.on(admin_cmd(pattern=r"img(?: |$)(\d*)? ?(.*)"))
+@bot.on(sudo_cmd(pattern=r"img(?: |$)(\d*)? ?(.*)", allow_sudo=True))
 async def img_sampler(event):
-    await event.edit("`ᴅᴀɪsʏ x ɪs ᴘʀᴏᴄᴇssɪɴɢ ᴛᴏ ғᴜɴᴅ ᴛʜᴇ ɪᴍᴀɢᴇs`")
-    reply = await event.get_reply_message()
-    if event.pattern_match.group(1):
-        query = event.pattern_match.group(1)
-    elif reply:
-        query = reply.message
-    else:
-        await event.edit("`um, mind mentioning what I actually need to search for ;_;`")
+    if event.fwd_from:
         return
-
-    lim = findall(r"lim=\d+", query)
-    # lim = event.pattern_match.group(1)
-    try:
-        lim = lim[0]
-        lim = lim.replace("lim=", "")
-        query = query.replace("lim=" + lim[0], "")
-    except IndexError:
-        lim = 5
+    reply_to_id = await reply_id(event)
+    if event.is_reply and not event.pattern_match.group(2):
+        query = await event.get_reply_message()
+        query = str(query.message)
+    else:
+        query = str(event.pattern_match.group(2))
+    if not query:
+        return await edit_or_reply(
+            event, "Reply to a message or pass a query to search!"
+        )
+    cat = await edit_or_reply(event, "`**Processing...**`")
+    if event.pattern_match.group(1) != "":
+        lim = int(event.pattern_match.group(1))
+        if lim > 10:
+            lim = int(10)
+        if lim <= 0:
+            lim = int(1)
+    else:
+        lim = int(3)
     response = googleimagesdownload()
-
     # creating list of arguments
     arguments = {
         "keywords": query,
@@ -40,12 +38,21 @@ async def img_sampler(event):
         "format": "jpg",
         "no_directory": "no_directory",
     }
-
     # passing the arguments to the function
-    paths = response.download(arguments)
+    try:
+        paths = response.download(arguments)
+    except Exception as e:
+        return await cat.edit(f"Error: \n`{e}`")
     lst = paths[0][query]
-    await event.client.send_file(
-        await event.client.get_input_entity(event.chat_id), lst
-    )
+    await event.client.send_file(event.chat_id, lst, reply_to=reply_to_id)
     shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
-    await event.delete()
+    await cat.delete()
+
+
+CMD_HELP.update(
+    {
+        "images": "**Plugin :**`images`\
+    \n\n**  •  Syntax :** `.img <limit> <Name>` or `.img <limit> (replied message)`\
+    \n**  •  Function : **do google image search and sends 3 images. default if you havent mentioned limit"
+    }
+)
