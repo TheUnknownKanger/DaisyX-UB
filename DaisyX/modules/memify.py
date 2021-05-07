ac6c79927ec16c6b55be793661012b04dbf0aabf
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-07 09:13:16 +0530

    Update memify.py

--- DaisyX/modules/memify.py
+++ DaisyX/modules/memify.py
@@ -47,7 +47,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "arial.ttf"
     else:
-        fnt = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+        fnt = "Resources/DejaVuSans.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if "," in text:
         upper_text, lower_text = text.split(",")
