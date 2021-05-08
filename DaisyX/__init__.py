27f95be97f2a69267b40524a1a930f2e42d84b40
PROBOY X <68194873+PROBOYX@users.noreply.github.com>
2021-05-08 09:20:05 +0530

    Update __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -48,7 +48,9 @@ def HELP(**DAISYX):
     else:
         CMD_HELP.update({see: helper})
     CMD_HELP.update({see: helper})
-
+def PROBOYX (event):
+  I_AM_PROBOY_X = event.text.split(" ", maxsplit=1)[1]
+  return I_AM_PROBOY_X
 
 DAISY_ID = ["1557333374"]
 
