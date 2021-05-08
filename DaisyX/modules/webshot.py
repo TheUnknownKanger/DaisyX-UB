c61b9862cd11fff5a24059b5d39c1f775a9b4d0c
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-08 09:01:28 +0530

    Update and rename webshot to webshot.py

---
+++ DaisyX/modules/webshot.py
@@ -0,0 +1,20 @@
+# (c) Copyright 2021-2022 DaisyX
+# Made By Sipak_Op, Devil and InukaAsith
+
+import requests
+from DaisyX import bot
+from telethon import events
+from DaisyX.utils import admin_cmd
+
+@bot.on(admin_cmd(pattern="webss"))
+async def events(event):
+  BASE = 'https://render-tron.appspot.com/screenshot/'
+  url = event.pattern_match.group(1)
+  path = 'target.jpg'
+  response = requests.get(BASE + url, stream=True)
+  if response.status_code == 200:
+      with open(path, 'wb') as file:
+          for chunk in response:
+              file.write(chunk)
+
+  await borg.send_file(event.chat_id, path)
