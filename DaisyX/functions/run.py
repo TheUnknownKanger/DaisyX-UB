6cfa2b339c66c7d6c733b750e4e1a6ed69d0bda1
Inuka Asith <inukaasith7@gmail.com>
2021-05-08 19:13:25 +0530

    Create run.py

---
+++ DaisyX/functions/run.py
@@ -0,0 +1,20 @@
+
+import asyncio
+import glob
+import ntpath
+import shlex
+from typing import Tuple
+
+async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
+    """Run Commands"""
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
