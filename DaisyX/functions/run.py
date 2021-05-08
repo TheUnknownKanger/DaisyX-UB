8d6ab7dc43b7b46ad3c384c34ce7a34eaee01b15
Inuka Asith <inukaasith7@gmail.com>
2021-05-08 19:27:30 +0530

    Update run.py

--- DaisyX/functions/run.py
+++ DaisyX/functions/run.py
@@ -5,13 +5,13 @@ import ntpath
 import shlex
 from typing import Tuple
 
-async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
+def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
     """Run Commands"""
     args = shlex.split(cmd)
-    process = await asyncio.create_subprocess_exec(
+    process = asyncio.create_subprocess_exec(
         *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
-    stdout, stderr = await process.communicate()
+    stdout, stderr = process.communicate()
     return (
         stdout.decode("utf-8", "replace").strip(),
         stderr.decode("utf-8", "replace").strip(),
