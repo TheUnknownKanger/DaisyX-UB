db7ed6c475266db699e6831b8b03dcdad4d1064e
Inuka Asith <inukaasith7@gmail.com>
2021-05-09 19:53:51 +0530

    Create __init__.py

---
+++ DaisyX/functions/__init__.py
@@ -0,0 +1,158 @@
+import asyncio
+import json
+import math
+import os
+import re
+import shlex
+import subprocess
+import time
+import webbrowser
+from os.path import basename
+from pathlib import Path
+from typing import List, Optional, Tuple, Union
+
+import hachoir
+import requests
+import telethon
+from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from pymediainfo import MediaInfo
+from telethon import Button, custom, events, functions
+from telethon.tl.types import (
+    DocumentAttributeAudio,
+    InputMessagesFilterDocument,
+    MessageMediaPhoto,
+)
+from youtube_dl import YoutubeDL
+from youtube_dl.utils import (
+    ContentTooShortError,
+    DownloadError,
+    ExtractorError,
+    GeoRestrictedError,
+    MaxDownloadsReached,
+    PostProcessingError,
+    UnavailableVideoError,
+    XAttrMetadataError,
+)
+
+import os
+import zipfile
+
+import aiohttp
+
+session = aiohttp.ClientSession()
+
+sedpath = "./"
+from DaisyX import logging
+
+logger = logging.getLogger("[--WARNING--]")
+if not os.path.isdir(sedpath):
+    os.makedirs(sedpath)
+
+    
+# Thanks To Userge-X
+# Ported By @STARKXD
+async def convert_to_image(event, borg):
+    lmao = await event.get_reply_message()
+    if not (
+        lmao.gif
+        or lmao.audio
+        or lmao.voice
+        or lmao.video
+        or lmao.video_note
+        or lmao.photo
+        or lmao.sticker
+        or lmao.media
+    ):
+        await event.edit("`Format Not Supported.`")
+        return
+    else:
+        try:
+            c_time = time.time()
+            downloaded_file_name = await borg.download_media(
+                lmao.media,
+                sedpath,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, event, c_time, "`Downloading...`")
+                ),
+            )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await event.edit(str(e))
+        else:
+            await event.edit(
+                "Downloaded to `{}` successfully.".format(downloaded_file_name)
+            )
+    if not os.path.exists(downloaded_file_name):
+        await event.edit("Download Unsucessfull :(")
+        return
+    if lmao and lmao.photo:
+        lmao_final = downloaded_file_name
+    elif lmao.sticker and lmao.sticker.mime_type == "application/x-tgsticker":
+        rpath = downloaded_file_name
+        image_name20 = os.path.join(sedpath, "SED.png")
+        cmd = f"lottie_convert.py --frame 0 -if lottie -of png {downloaded_file_name} {image_name20}"
+        stdout, stderr = (await runcmd(cmd))[:2]
+        os.remove(rpath)
+        lmao_final = image_name20
+    elif lmao.sticker and lmao.sticker.mime_type == "image/webp":
+        pathofsticker2 = downloaded_file_name
+        image_new_path = sedpath + "image.png"
+        os.rename(pathofsticker2, image_new_path)
+        if not os.path.exists(image_new_path):
+            await event.edit("`Wasn't Able To Fetch Shot.`")
+            return
+        lmao_final = image_new_path
+    elif lmao.audio:
+        sed_p = downloaded_file_name
+        hmmyes = sedpath + "stark.mp3"
+        imgpath = sedpath + "starky.jpg"
+        os.rename(sed_p, hmmyes)
+        await runcmd(f"ffmpeg -i {hmmyes} -filter:v scale=500:500 -an {imgpath}")
+        os.remove(sed_p)
+        if not os.path.exists(imgpath):
+            await event.edit("`Wasn't Able To Fetch Shot.`")
+            return
+        lmao_final = imgpath
+    elif lmao.gif or lmao.video or lmao.video_note:
+        sed_p2 = downloaded_file_name
+        jpg_file = os.path.join(sedpath, "image.jpg")
+        await take_screen_shot(sed_p2, 0, jpg_file)
+        os.remove(sed_p2)
+        if not os.path.exists(jpg_file):
+            await event.edit("`Couldn't Fetch. SS`")
+            return
+        lmao_final = jpg_file
+    await event.edit("`Almost Completed.`")
+    return lmao_final
+
+# Thanks To Userge-X
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """run command in terminal"""
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+  
+# Thanks To Userge-X
+async def crop_vid(input_vid: str, final_path: str):
+    media_info = MediaInfo.parse(input_vid)
+    for track in media_info.tracks:
+        if track.track_type == "Video":
+            aspect_ratio = track.display_aspect_ratio
+            height = track.height
+            width = track.width
+    if aspect_ratio != 1:
+        crop_by = width if (height > width) else height
+        os.system(f'ffmpeg -i {input_vid} -vf "crop={crop_by}:{crop_by}" {final_path}')
+        os.remove(input_vid)
+    else:
+        os.rename(input_vid, final_path)
