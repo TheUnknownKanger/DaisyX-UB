b32d9528952f0b9ff2a1d264a6a83aeae93ad138
Alain <77918121+infotechbro@users.noreply.github.com>
2021-05-12 18:07:13 +0530

    Update ping.py

--- DaisyX/modules/ping.py
+++ DaisyX/modules/ping.py
@@ -1,9 +1,11 @@
 import asyncio
 from datetime import datetime
 
-from .. import ALIVE_NAME, CMD_HELP
+from DaisyX import ALIVE_NAME, CMD_HELP
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
+
+# ting by madboy 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "DaisyX"
 
 from heroku_config import Var as Config
