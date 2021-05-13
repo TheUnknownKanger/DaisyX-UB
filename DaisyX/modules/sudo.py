8d6d5acecd0c5637f0b0663c05e2fc65832ba756
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-13 07:30:38 +0530

    Update sudo.py

--- DaisyX/modules/sudo.py
+++ DaisyX/modules/sudo.py
@@ -14,7 +14,7 @@ Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
 sudousers = os.environ.get("SUDO_USERS", None)
 
-@bot.on(admin_cmd(pattern="addsudo")) 
+@bot.on(admin_cmd(pattern="sudo")) 
 async def sudo(events):
      sudolelo = os.environ.get("SUDO_USERS", None) 
      await eor(event, f"** sᴜᴅᴏʟɪsᴛ ᴏғ ** bot.me.username\n {sudolelo}")
