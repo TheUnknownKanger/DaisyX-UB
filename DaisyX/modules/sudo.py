7149a43726b5b12a4743c7870cbf76a859599d5c
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-13 09:44:02 +0530

    Update sudo.py

--- DaisyX/modules/sudo.py
+++ DaisyX/modules/sudo.py
@@ -18,5 +18,4 @@ sudousers = os.environ.get("SUDO_USERS", None)
 @bot.on(admin_cmd(pattern="sudo")) 
 async def sudo(events):
      sudolelo = os.environ.get("SUDO_USERS", None) 
-     chat = event.chat.id
-     await bot.send_message(chat, f"** sᴜᴅᴏʟɪsᴛ ᴏғ ** bot.me.username\n {sudolelo}")
+     await bot.send_message(f"** sᴜᴅᴏʟɪsᴛ ᴏғ ** bot.me.username\n {sudolelo}")
